ea4a0a22e1d0d1bd9c09036b52378296d91451f6
simba0905 <rascalsimba0905@gmail.com>
2018-11-26 06:04:40 +0300

    get fulfillment order

--- app/Http/Controllers/OrderCount.php
+++ app/Http/Controllers/OrderCount.php
@@ -49,25 +49,51 @@ class OrderCount extends Controller
 
     	if (!$country) return false;
 
-    	// Gets a list of orders.
-	    $results = $this->shopify->call([ 
-	        'METHOD'    => 'GET', 
-	        'URL'       => '/admin/orders.json'
-	    ]);
-
+    	$total_order = $this->getTotalOrderCounts();
+    	$limit = 50;
+    	$page = 1;
+    	
 	    // Order Count based on this country.
 	    $order_count = 0;
-	    foreach($results->orders as $result) {
-	    	if ($country == $result->customer->default_address->country) {
-	    		$order_count++;
+
+	    if ($total_order > $limit) {
+	    	$page_count = intval($total_order / $limit) + 1;
+
+	    	for($i = 1; $i <= $page_count; $i++) {
+	    		$url  = '/admin/orders.json?fields=customer&fulfillment_status=shipped&limit=' . $limit . '&page=' . $i;
+	    		$results = $this->shopify->call([ 
+			        'METHOD'    => 'GET', 
+			        'URL'       => $url
+			    ]);
+
+			    foreach($results->orders as $result) {
+			    	if ($country == $result->customer->default_address->country) {
+			    		$order_count++;
+			    	}
+			    }
 	    	}
+
+	    } else {
+	    	$url  = '/admin/orders.json?fields=customer&fulfillment_status=shipped&limit=' . $limit . '&page=' . $page;
+
+	    	// Gets a list of orders.
+		    $results = $this->shopify->call([ 
+		        'METHOD'    => 'GET', 
+		        'URL'       => $url
+		    ]);
+
+		    foreach($results->orders as $result) {
+		    	if ($country == $result->customer->default_address->country) {
+		    		$order_count++;
+		    	}
+		    }
 	    }
 
 	    $data = [
 	    	'country'		=> $country,
 	    	'ip'	 		=> $ip,
 	    	'order_count'	=> $order_count,
-	    	'total_order'	=> count($results->orders)
+	    	'total_order'	=> $total_order
 	    ];
 
 	    return $data;
